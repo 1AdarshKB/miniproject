22efe0088d8bc8bbcd8eecb6f317e3adb8b45c4c
1AdarshKB <169120756+1AdarshKB@users.noreply.github.com>
2024-05-07 14:06:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,10 @@ fet();
 
 function fet(){
     console.log("ping")
-    fetch("https://dummytest.pythonanywhere.com/status")
+    fetch("http://dummytest.pythonanywhere.com/status")
     .then((res)=>(res.json()))
     .then((json)=>{
-        console.log(json.access_status)
-        if(json.access_status){
+                if(json.access_status){
             document.getElementById("status").innerHTML="Online";
             document.getElementById("status").style.color="green";
         }
@@ -15,20 +14,55 @@ function fet(){
             document.getElementById("status").style.color="red";
         }
     })
-    fetch("https://dummytest.pythonanywhere.com/get_data")
+    fetch("http://dummytest.pythonanywhere.com/get_data")
     .then((res)=> (res.json()))
     .then((json)=>{
         var data=json.data;
         var data_len=data.length;
         var box=["temp","humi","heart","spo","date"];
+var avg_temp=0
+var avg_heart=0;
 
 
         for(var i=0;i<=box.length-1;i++){
-            document.getElementById(box[i]).innerHTML="<td>"+data[i][0]+"</td>"
+            document.getElementById(box[i]).innerHTML="<td>"+data[i][0]+"</td>";
+		avg_heart=avg_heart+Number(data[i][2]);
+		avg_temp=avg_temp+Number(data[i][0]);
+		
             for(var j=1;j<=data_len-1;j++){
                 document.getElementById(box[i]).innerHTML+="<td>"+data[i][j]+"</td>"
             }
         }
+	heart=avg_heart/5;
+	temp=avg_temp/5;
+console.log(heart);
+console.log(temp);
+
+	if(heart <= 60){
+	document.getElementById("intense").innerHTML="Low";
+	document.getElementById("intense").style.color="green";
+	}
+	if(heart >= 100){
+	document.getElementById("intense").innerHTML="High";
+	document.getElementById("intense").style.color="red";
+	}
+	if(heart >= 60 && heart <= 100){
+	document.getElementById("intense").innerHTML="Normal";
+	document.getElementById("intense").style.color="yellow";
+	}
+	if(temp <= 30){
+	document.getElementById("tempintense").innerHTML="Low";
+	document.getElementById("tempintense").style.color="green";
+	}
+	if(temp >= 40){
+	document.getElementById("tempintense").innerHTML="High";
+	document.getElementById("tempintense").style.color="red";
+	}
+	if(temp >= 30 && temp <= 40){
+	document.getElementById("tempintense").innerHTML="Normal";
+	document.getElementById("tempintense").style.color="yellow";
+
+	}
 
     })
 }
